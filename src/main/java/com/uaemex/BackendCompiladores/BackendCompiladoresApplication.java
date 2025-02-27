4e84ff5c311ae8f9bc8d210434c13f18cf218f87
ImMateo <matthew.fco.14@gmail.com>
2025-02-27 08:22:16 -0600

    Archivos subidos

---
+++ src/main/java/com/uaemex/BackendCompiladores/BackendCompiladoresApplication.java
@@ -0,0 +1,13 @@
+package com.uaemex.BackendCompiladores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCompiladoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCompiladoresApplication.class, args);
+	}
+
+}
